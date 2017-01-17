5eb71fed830616d5da39e95c19573a38f2814d8c
jeongmoon417 <jm5836@naver.com>
2017-01-17 18:02:17 +0900

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,11 @@
+while True:
+        s=raw_input('Enter something: ')
+        if s == 'quit' :
+                break
+        if len(s) <3:
+                print 'Too small'
+                continue
+                #continue : not excute lower line, go to next loop
+        print 'Input is of sufficient lenth'
+
+
---
+++ for.py
@@ -0,0 +1,7 @@
+for i in range(1,5):
+        #range(1,5) returns list [1,2,3,4]
+        #range(1,5,2) returns list [1,3] : increase amount=2
+        print i
+else:
+        print 'The for loop is over'
+
