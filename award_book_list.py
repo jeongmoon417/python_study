97e64729c536a377c7c9dc0e267cd31acd03a8cc
jeongmoon417 <jm5836@naver.com>
2017-02-15 18:54:58 +0900

    Add files via upload

---
+++ award_book_list.py
@@ -0,0 +1,19 @@
+import csv
+
+books = []
+
+f=open('C:\jmoon\goodreads_choice_award_2016_CSV.csv','r')
+csvReader = csv.reader(f)
+
+print csvReader
+
+for row in csvReader:
+	books.append(row)
+
+print "There are %d books in list" %(len(books))
+
+
+for i in range(len(books)):
+    print books[i]
+
+f.close
