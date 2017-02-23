18019dd987832a88166ae90f920d807e35f96154
jeongmoon417 <jm5836@naver.com>
2017-02-23 17:18:03 +0900

    Update ds_reference.py

--- ds_reference.py
+++ ds_reference.py
@@ -3,7 +3,7 @@
 print 'Simple Assignment'
 shopping_list=['apple', 'banana', 'mango', 'strawberry']
 
-mylist = shopping_list
+mylist = shopping_list  #pass reference
 
 del shopping_list[0]
 
@@ -11,7 +11,7 @@ print 'Shopping list is', shopping_list     #['banana', 'mango', 'strawberry']
 print 'My list is', mylist      #['banana', 'mango', 'strawberry']
 
 print 'Copy by making a full slice'
-mylist = shopping_list[:]
+mylist = shopping_list[:] #pass value
 del mylist[0]
 
 print 'Shopping list is', shopping_list     #['banana', 'mango', 'strawberry']
