7f9959a8425e63e402295819239e7dc3adc4673d
jeongmoon417 <jm5836@naver.com>
2017-02-28 12:38:48 +0900

    Update scatter2.py

--- scatter2.py
+++ scatter2.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 '''
-error
+
+(1,1),(2,5),(3,6) 빨간 별모양 점찍기
+scatter 함수 이용
+
+ERROR
 Traceback (most recent call last):
   File "scatter2.py", line 20, in <module>
     plt.scatter([(1,1)], 50, color ='red')
@@ -13,6 +17,8 @@ Traceback (most recent call last):
     raise ValueError("x and y must be the same size")
 ValueError: x and y must be the same size
 
+ERROR FIXED : chaged value of x, y in fuction scatter
+              scatter([(1,1), (2,5), (3,6)], 50, color ='red')  ---> plt.scatter([1,2,3],[1,5,6], 100, color ='red')
 
 '''
 import sys
@@ -28,6 +34,6 @@ plt.switch_backend('agg')
 fig = plt.figure(figsize=(8,6), dpi=80)
 
 # 정점 찍기
-plt.scatter([(1,1)], 50, color ='red')
+plt.scatter([1,2,3],[1,5,6], 100, color ='red',marker='*')
 
 fig.savefig('scatter2.png')
