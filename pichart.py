e2e2bea9b13da39befb529f51ab370b4031d2c86
jeongmoon417 <jm5836@naver.com>
2017-02-28 16:07:13 +0900

    Add files via upload

---
+++ pichart.py
@@ -0,0 +1,22 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+'''
+원그래프 그리기
+'''
+
+import matplotlib.pyplot as plt
+
+plt.switch_backend('agg')
+fig = plt.figure()
+
+labels = 'Mango', 'Strawberry', 'Graph', 'Banana', 'Tomato'
+sizes = [12, 37, 29, 13, 5]
+colors = ['#ff9e9b', '#ffa8a5',  '#ffb2af',  '#ffbcb9',  '#ffc6c3']
+explode = (0, 0.1, 0, 0, 0)
+
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.axis('equal')
+
+fig.savefig('piechart.png')
