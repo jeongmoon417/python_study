e14f199d4af713211cd072838f1576e38c54be1e
jeongmoon417 <jm5836@naver.com>
2017-03-08 23:44:50 +0900

    Update beak1.py

--- beak1.py
+++ beak1.py
@@ -18,17 +18,20 @@ chapters = [40, 30, 30, 50]
 sum = 0
 for i in range(len(chapters)):
     chapters.sort()
-    if len(chapters) == 1:
+    print chapters
+    
+    if len(chapters) == 2:
+        sum += chapters[0]
+        sum += chapters[1]
         break
+        
     two_chapter = chapters[0] + chapters[1]
     print chapters[0], 'and', chapters[1], 'merged'
     sum += two_chapter
     print chapters[0] , 'deleted'
     del chapters[0]
-    if (len(chapters) > 1):
-        print chapters[0], 'deleted'
-        del chapters[0]
+    print chapters[0], 'deleted'
+    del chapters[0]
     chapters.append(two_chapter)
-    print chapters
 
 print sum
